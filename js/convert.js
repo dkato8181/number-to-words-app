f351f1e5b715d9c0a46c6ad6ca04f78d33de117f
davidk <dkato81@gmail.com>
2024-05-08 06:20:55 +0300

    move js to seperate files

---
+++ js/convert.js
@@ -0,0 +1,106 @@
+const ones = {
+    0: '',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'forteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eigteen',
+    19: 'ninteen'
+};
+const tens = {
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety',
+}
+const thousands = {
+    0: 'hundred',
+    1: 'thousand',
+    2: 'million',
+    3: 'billion',
+    4: 'trillion',
+}
+const MIN = 0;
+const MAX = 999_999_999_999_999;
+
+function convert(num) {
+    if (num == 0) {
+        return 'zero';
+    }
+
+    let end = num.toString().length;
+    let start;
+    let words = '';
+    let i = 0;
+
+    while (end > 0) {
+        let hundredth = '';
+        let thousandth = '';
+        let remainder = '';
+
+        start = end - 3;
+
+        if (start < 0) {
+            start = 0;
+        }
+
+        let slice = num.toString().slice(start, end);
+        let a = Math.floor(slice / 100);
+        let b = slice % 100;
+
+        if (a > 0) {
+            hundredth = `${getWords(a)} ${thousands[0]}`;
+        }
+
+        remainder = getWords(b);
+        thousandth = `${hundredth} ${remainder}`;
+
+        if (i > 0 && thousandth.trim().length != 0) {
+            words = `${thousandth} ${thousands[i]} ${words}`;
+        }
+        else {
+            words = `${thousandth} ${words}`
+        }
+
+        if (start == 0) {
+            break;
+        }
+
+        i++;
+        end = start;
+    }
+
+    return words.trim();
+}
+
+function getWords(number) {
+    let text = '';
+
+    if (number < 20) {
+        text = ones[number];
+    }
+    else {
+        let oneth = number % 10;
+        let tenth = Math.floor(number / 10);
+        text = `${tens[tenth]} ${ones[oneth]}`;
+    }
+
+    return text.trim();
+}
---
+++ js/main.js
@@ -0,0 +1,22 @@
+const buttonConvert = document.getElementById("convert");
+const pWords = document.getElementById("words");
+const inputNumber = document.getElementById("number");
+
+buttonConvert.addEventListener("click", convertListener);
+
+function convertListener(e) {
+    const number = parseInt(inputNumber.value);
+    let words;
+
+    if (isNaN(number)) {
+        words = 'value is not a number';
+    }
+    else if (number < MIN || number > MAX) {
+        words = 'number not yet supported';
+    }
+    else {
+        words = convert(number);
+    }
+
+    pWords.innerHTML = words;
+}
